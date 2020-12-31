e6630e619ecb61aeb7bb7390ab1524c5437469d3
Anastasiia <nuka97@yandex.ru>
2020-12-31 12:31:59 +0300

    refactoring and params changes

--- classificator_with_learning.py
+++ classificator_with_learning.py
@@ -19,8 +19,8 @@ from xml.dom import minidom
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
-emotions_threshold = 30
-valence_threshold = 20
+emotions_threshold = 20
+valence_threshold = 30
 
 training_data = []
 training_ids = []
@@ -48,6 +48,16 @@ test_joy_labels = []
 test_sadness_labels = []
 test_surprise_labels = []
 
+result_valence_labels = []
+result_triple_valence_labels = []
+
+result_anger_labels = []
+result_disgust_labels = []
+result_fear_labels = []
+result_joy_labels = []
+result_sadness_labels = []
+result_surprise_labels = []
+
 def preprocessPhrase(phrase):
     #remove new lines
     phrase = phrase.replace('\n', '')
@@ -68,10 +78,6 @@ def preprocessPhrase(phrase):
         res+=word + " "
     return res
 
-pos = 0
-neg = 0
-neut = 0
-
 #if value > threshold then emotion exists
 #threshold is in range [0, 100]
 def emotionToBinary(value, threshold):
@@ -85,28 +91,19 @@ def emotionToBinary(value, threshold):
 #[-threshold, threshold] -> neutral
 #[threshold, 100] -> positive
 def getLabelTriple(value, threshold):
-    global pos
-    global neg
-    global neut
     if (value >= threshold):
-        pos+=1
-        return "1"
+        return 1
     elif (value < threshold) and (value > -1*threshold):
-        neut+=1
-        return "0"
+        return 0
     else:
-        neg+=1
-        return "-1"
+        return -1
 
 #Positive or negative
 def getLabelBinary(value):
-    global pos
-    global neg
-    global neut
     if (value > 0):
-        return "1"
+        return 1
     else:
-        return "0"
+        return 0
 
 def loadXmlData(xmlFilePath, ids, data):
     xmldoc = minidom.parse(xmlFilePath)
@@ -122,6 +119,7 @@ def loadValenceLabelsBinary(labelsFilePath, labels):
     file_labels = f.readlines()
     for label in file_labels:
         labels.append(getLabelBinary(int(label.split()[1])))
+    f.close()
 
 #Load labels for positive, neutral and negative classification
 def loadValenceLabelsTriple(labelsFilePath, labels):
@@ -129,6 +127,7 @@ def loadValenceLabelsTriple(labelsFilePath, labels):
     file_labels = f.readlines()
     for label in file_labels:
         labels.append(getLabelTriple(int(label.split()[1]), valence_threshold))
+    f.close()
 
 
 #Load test labels for all emotions
@@ -143,6 +142,7 @@ def loadEmotionsLabelsTraining(labelsFilePath):
         training_joy_labels.append(emotionToBinary(int(emotions[4]), emotions_threshold))
         training_sadness_labels.append(emotionToBinary(int(emotions[5]), emotions_threshold))
         training_surprise_labels.append(emotionToBinary(int(emotions[6]), emotions_threshold))
+    f.close()
 
 #Load test labels for all emotions
 def loadEmotionsLabelsTest(labelsFilePath):
@@ -156,6 +156,7 @@ def loadEmotionsLabelsTest(labelsFilePath):
         test_joy_labels.append(emotionToBinary(int(emotions[4]), emotions_threshold))
         test_sadness_labels.append(emotionToBinary(int(emotions[5]), emotions_threshold))
         test_surprise_labels.append(emotionToBinary(int(emotions[6]), emotions_threshold))
+    f.close()
 
 def writeValenceResultFile(test_ids, valence_result_file_path, labels):
     fileValence = open(valence_result_file_path, 'w+')
@@ -168,70 +169,75 @@ def writeEmotionsResultFile(test_ids, emotions_result_file_path):
     for i in range(len(test_ids)):
         fileEmotions.write('{} {} {} {} {} {} {}\n'.format(
             test_ids[i], 
-            test_anger_labels[i], 
-            test_disgust_labels[i],
-            test_fear_labels[i],
-            test_joy_labels[i],
-            test_sadness_labels[i],
-            test_surprise_labels[i]
+            result_anger_labels[i], 
+            result_disgust_labels[i],
+            result_fear_labels[i],
+            result_joy_labels[i],
+            result_sadness_labels[i],
+            result_surprise_labels[i]
         ))
     fileEmotions.close()
 
-def classify(training_data, test_data, training_labels, test_labels):
+#classifier_to_save_to_run is a number of classifier to run
+# 0 => SVM, kernel=rbf
+# 1 => SVM kernel=poly
+# 2 => SVM kernel=sigmoid
+# 3 => SVM kernel=linear
+# 4 => LinearSVC
+def classify(training_data, test_data, training_labels, test_labels, classifier_to_run = 0):
+    result_labels = []
     #vectorization with tfid
-    vectorizer = TfidfVectorizer(ngram_range = (1,2), min_df=3, max_df = 0.8, sublinear_tf = True)
+    vectorizer = TfidfVectorizer(ngram_range = (1,3), min_df=3, max_df = 0.8, sublinear_tf = True)
     train_vectors = vectorizer.fit_transform(training_data)
     test_vectors = vectorizer.transform(test_data)
 
-    #SVM, kernel=rbf
-    classifier_rbf = svm.SVC()
-    classifier_rbf.fit(train_vectors, training_labels)
+    if classifier_to_run == 0:
+        #SVM, kernel=rbf
+        classifier_rbf = svm.SVC()
+        classifier_rbf.fit(train_vectors, training_labels)
+        prediction_rbf = classifier_rbf.predict(test_vectors)
+        result_labels = prediction_rbf
+        print("===========================SVC(kernel=rbf)===========================")
+        print(confusion_matrix(test_labels, prediction_rbf))
+        print(classification_report(test_labels, prediction_rbf))
 
-    #SVM, kernel=rbf
-    classifier_rbf = svm.SVC()
-    classifier_rbf.fit(train_vectors, training_labels)
-    prediction_rbf = classifier_rbf.predict(test_vectors)
-
-    #SVM kernel=poly
-    classifier_poly = svm.SVC(kernel='poly')
-    classifier_poly.fit(train_vectors, training_labels)
-    prediction_poly = classifier_poly.predict(test_vectors)
-
-    #SVM kernel=sigmoid
-    classifier_sigmoid = svm.SVC(kernel='sigmoid')
-    classifier_sigmoid.fit(train_vectors, training_labels)
-    prediction_sigmoid = classifier_sigmoid.predict(test_vectors)
-
-    #SVM kernel=linear
-    classifier_linear = svm.SVC(kernel='linear')
-    classifier_linear.fit(train_vectors, training_labels)
-    prediction_linear = classifier_linear.predict(test_vectors)
-
-    #LinearSVC
-    classifier_liblinear = svm.LinearSVC()
-    classifier_liblinear.fit(train_vectors, training_labels)
-    prediction_liblinear = classifier_liblinear.predict(test_vectors)
-
-    print("===========================SVC(kernel=rbf)===========================")
-    print(confusion_matrix(test_labels, prediction_rbf))
-    print(classification_report(test_labels, prediction_rbf))
-
-    print("=========================== SVC(kernel=poly) ===========================")
-    print(confusion_matrix(test_labels, prediction_poly))
-    print(classification_report(test_labels, prediction_poly))
-
-    print("=========================== SVC(kernel=sigmoid) ===========================")
-    print(confusion_matrix(test_labels, prediction_sigmoid))
-    print(classification_report(test_labels, prediction_sigmoid))
-
-    print("=========================== SVC(kernel=linear) ===========================")
-    print(confusion_matrix(test_labels, prediction_linear))
-    print(classification_report(test_labels, prediction_linear))
-
-    print("=========================== LinearSVC ===========================")
-    print(confusion_matrix(test_labels, prediction_liblinear))
-    print(classification_report(test_labels, prediction_liblinear))
-
+    elif classifier_to_run == 1:
+        #SVM kernel=poly
+        classifier_poly = svm.SVC(kernel='poly')
+        classifier_poly.fit(train_vectors, training_labels)
+        prediction_poly = classifier_poly.predict(test_vectors)
+        result_labels = prediction_poly
+        print("=========================== SVC(kernel=poly) ===========================")
+        print(confusion_matrix(test_labels, prediction_poly))
+        print(classification_report(test_labels, prediction_poly))
+    elif classifier_to_run == 2:
+        #SVM kernel=sigmoid
+        classifier_sigmoid = svm.SVC(kernel='sigmoid')
+        classifier_sigmoid.fit(train_vectors, training_labels)
+        prediction_sigmoid = classifier_sigmoid.predict(test_vectors)
+        result_labels = prediction_sigmoid
+        print("=========================== SVC(kernel=sigmoid) ===========================")
+        print(confusion_matrix(test_labels, prediction_sigmoid))
+        print(classification_report(test_labels, prediction_sigmoid))
+    elif classifier_to_run == 3:
+        #SVM kernel=linear
+        classifier_linear = svm.SVC(kernel='linear')
+        classifier_linear.fit(train_vectors, training_labels)
+        prediction_linear = classifier_linear.predict(test_vectors)
+        result_labels = prediction_linear
+        print("=========================== SVC(kernel=linear) ===========================")
+        print(confusion_matrix(test_labels, prediction_linear))
+        print(classification_report(test_labels, prediction_linear))
+    else:
+        #LinearSVC
+        classifier_liblinear = svm.LinearSVC()
+        classifier_liblinear.fit(train_vectors, training_labels)
+        prediction_liblinear = classifier_liblinear.predict(test_vectors)
+        result_labels = prediction_liblinear
+        print("=========================== LinearSVC ===========================")
+        print(confusion_matrix(test_labels, prediction_liblinear))
+        print(classification_report(test_labels, prediction_liblinear))
+    return result_labels
     
 valence_test_file = "datasets/AffectiveText.test/affectivetext_test.valence.gold"
 emotions_test_file = "datasets/AffectiveText.test/affectivetext_test.emotions.gold"
@@ -248,16 +254,16 @@ loadXmlData('datasets/AffectiveText.trial/affectivetext_trial.xml', training_ids
 loadXmlData('datasets/AffectiveText.test/affectivetext_test.xml', test_ids, test_data)
 
 #Valence binary (positive and negative)
-loadValenceLabelsBinary('datasets/AffectiveText.trial/affectivetext_trial.valence.gold', valence_training_labels)
+loadValenceLabelsBinary(valence_trial_file, valence_training_labels)
 loadValenceLabelsBinary(valence_test_file, valence_test_labels)
 print('================================================================= Sentiment analysis (binary) =================================================================')
-classify(training_data, test_data, valence_training_labels, valence_test_labels)
+result_valence_labels = classify(training_data, test_data, valence_training_labels, valence_test_labels)
 
 #Valence triple (positive, negative and neutral)
 loadValenceLabelsTriple(valence_trial_file, valence_triple_training_labels)
 loadValenceLabelsTriple(valence_test_file, valence_triple_test_labels)
 print('================================================================= Sentiment analysis (triple) =================================================================')
-classify(training_data, test_data, valence_triple_training_labels, valence_triple_test_labels)
+result_triple_valence_labels = classify(training_data, test_data, valence_triple_training_labels, valence_triple_test_labels)
 
 #Emotions recognition
 print('==================================================================== Emotions recognition =====================================================================')
@@ -266,33 +272,33 @@ loadEmotionsLabelsTest(emotions_test_file)
 print("=============================================================")
 print("=========================== Anger ===========================")
 print("=============================================================")
-classify(training_data, test_data, training_anger_labels, test_anger_labels)
+result_anger_labels = classify(training_data, test_data, training_anger_labels, test_anger_labels)
 
 print("=============================================================")
 print("=========================== Disgust =========================")
 print("=============================================================")
-classify(training_data, test_data, training_disgust_labels, test_disgust_labels)
+result_disgust_labels = classify(training_data, test_data, training_disgust_labels, test_disgust_labels)
 
 print("=============================================================")
 print("=========================== Fear =========================")
 print("=============================================================")
-classify(training_data, test_data, training_fear_labels, test_fear_labels)
+result_fear_labels = classify(training_data, test_data, training_fear_labels, test_fear_labels)
 
 print("=============================================================")
 print("=========================== Joy =========================")
 print("=============================================================")
-classify(training_data, test_data, training_joy_labels, test_joy_labels)
+result_joy_labels = classify(training_data, test_data, training_joy_labels, test_joy_labels)
 
 print("=============================================================")
 print("=========================== Sadness =========================")
 print("=============================================================")
-classify(training_data, test_data, training_sadness_labels, test_sadness_labels)
+result_sadness_labels = classify(training_data, test_data, training_sadness_labels, test_sadness_labels)
 
 print("=============================================================")
 print("=========================== Surprise =========================")
 print("=============================================================")
-classify(training_data, test_data, training_surprise_labels, test_surprise_labels)
+result_surprise_labels = classify(training_data, test_data, training_surprise_labels, test_surprise_labels)
 
 writeEmotionsResultFile(test_ids, emotions_SVM_result_file)
-writeValenceResultFile(test_ids, valence_SVM_result_file, valence_test_labels)
-writeValenceResultFile(test_ids, valence_triple_SVM_result_file, valence_triple_test_labels)
+writeValenceResultFile(test_ids, valence_SVM_result_file, result_valence_labels)
+writeValenceResultFile(test_ids, valence_triple_SVM_result_file, result_triple_valence_labels)
